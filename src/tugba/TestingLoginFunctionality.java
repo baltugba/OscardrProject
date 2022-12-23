9ef180054ae9522d85539e054d4910c6bb48e756
tt_ba <baltugba.usa@gmail.com>
2022-12-22 20:46:55 -0500

    test: qa-123 add testing off login functionality

---
+++ src/tugba/TestingLoginFunctionality.java
@@ -0,0 +1,7 @@
+package tugba;
+
+public class TestingLoginFunctionality {
+    public static void main(String[] args) {
+        System.out.println("I am testing Login functionality of my company");
+    }
+}
